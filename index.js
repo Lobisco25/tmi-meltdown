de7f53ca9ae18aa4beda57fdb63ff3329344add9
Lobisco25 <fefeeliaslobo@outlook.com>
2023-01-19 23:57:29 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const config = require("./config")
+
+const { ChatClient } = require("@kararty/dank-twitch-irc")
+
+const bottisco = new ChatClient(config.links.bottisco)
+
+const bottisco2 = new ChatClient(config.links.bottisco2)
+
+const lobisco25 = new ChatClient(config.links.lobisco25)
+
+bottisco.join(config.channel)
+
+bottisco2.join(config.channel)
+
+lobisco25.join(config.channel)
+
+bottisco.privmsg(config.channel, `helping tmi meltdown for ${config.channel} with ${Object.keys(config.links).length} accounts`)
+
+bottisco.on("PRIVMSG", async (msg) => {
+
+    const channel = msg.channelName
+
+    if (msg.messageText === "monkaS it's coming" && msg.senderUsername == config.warning) {
+        bottisco.privmsg(channel, "pajaGIGA pajbot")
+        bottisco2.privmsg(channel, "FeelsNotSureLady pajbot")
+        lobisco25.privmsg(channel, "PoroSad pls don't man pajbot")
+    }
+
+    else if (msg.messageText === "pajaS 🚨 ALERT" && msg.senderUsername == config.start) {
+        bottisco.privmsg(channel, "xdd 🚨 HERE IT COMES")
+        bottisco2.privmsg(channel, " pepeZ 🚨 *ignores*")
+        lobisco25.privmsg(channel, "FEELSDOEnkman GuitarTime 🚨 MUSIC TO MY EARS")
+    }
+})
