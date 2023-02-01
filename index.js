4416d2140c683e4c5027d9a5bc09cdffc7982572
Lobisco25 <fefeeliaslobo@outlook.com>
2023-02-01 15:48:46 -0300

    incidentbot added since it's now verified

--- index.js
+++ index.js
@@ -8,13 +8,17 @@ const bottisco2 = new ChatClient(config.links.bottisco2)
 
 const lobisco25 = new ChatClient(config.links.lobisco25)
 
+const incidentbot = new ChatClient(config.links.incidentbot)
+
 bottisco.join(config.channel)
 
 bottisco2.join(config.channel)
 
 lobisco25.join(config.channel)
 
-bottisco.privmsg(config.channel, `helping tmi meltdown for ${config.channel} with ${Object.keys(config.links).length} accounts`)
+incidentbot.join(config.channel)
+
+bottisco.privmsg(config.dev, `helping tmi meltdown for ${config.channel} with ${Object.keys(config.links).length} accounts`)
 
 bottisco.on("PRIVMSG", async (msg) => {
 
@@ -24,11 +28,13 @@ bottisco.on("PRIVMSG", async (msg) => {
         bottisco.privmsg(channel, "pajaGIGA pajbot")
         bottisco2.privmsg(channel, "FeelsNotSureLady pajbot")
         lobisco25.privmsg(channel, "PoroSad pls don't man pajbot")
+        incidentbot.privmsg(channel, "/me pajaMad pajbot")
     }
 
     else if (msg.messageText === "pajaS 🚨 ALERT" && msg.senderUsername == config.start) {
         bottisco.privmsg(channel, "xdd 🚨 HERE IT COMES")
         bottisco2.privmsg(channel, " pepeZ 🚨 *ignores*")
         lobisco25.privmsg(channel, "FEELSDOEnkman GuitarTime 🚨 MUSIC TO MY EARS")
+        incidentbot.privmsg(channel, "/me pajaNerd 🚨 EVERYBODY, TO THE PAJANERDSHIP!!!")
     }
 })
